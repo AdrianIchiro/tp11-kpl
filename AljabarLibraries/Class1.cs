ea49c4c9f738e06d6ba3877474fde10ddf24dafc
AdrianIchiro <adrianfahren10@gmail.com>
2024-05-05 15:07:17 +0700

    tp11

---
+++ AljabarLibraries/Class1.cs
@@ -0,0 +1,42 @@
+﻿namespace AljabarLibraries
+{
+    public class Aljabar
+    {
+        public static double[] AkarPersamaanKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+            double c = persamaan[2];
+
+            double discriminant = b * b - 4 * a * c;
+
+            if (discriminant > 0)
+            {
+                double root1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+                double root2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+                return new double[] { root1, root2 };
+            }
+            else if (discriminant == 0)
+            {
+                double root = -b / (2 * a);
+                return new double[] { root };
+            }
+            else
+            {
+                return new double[0];
+            }
+        }
+
+        public static double[] HasilKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+
+            double ad = a * a;
+            double ae_bd = 2 * a * b;
+            double bf_ce = b * b;
+
+            return new double[] { ad, ae_bd, bf_ce };
+        }
+    }
+}
---
+++ MainProgram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AljabarLibraries;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        double[] hasilAkar = Aljabar.AkarPersamaanKuadrat([1, -3, -10]);
+        foreach (var item in hasilAkar)
+        {
+            Console.Write(item + ", ");
+        }
+
+        Console.WriteLine();
+
+        double[] hasilKuadrat = Aljabar.HasilKuadrat([2, -3]);
+        foreach (var item in hasilKuadrat)
+        {
+            Console.Write(item + ", ");
+        }
+    }
+}
